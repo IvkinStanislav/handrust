0488455fa20daf88c4c33c828b6b862248c6b3ae
Stas Ivkin <109stas@gmail.com>
2024-12-29 22:13:42 +0300

    bad exchange

--- src/algos/greedy/exchange.rs
+++ src/algos/greedy/exchange.rs
@@ -1,5 +1,43 @@
-pub fn exchange(n: usize) -> Vec<Vec<usize>> {
-    todo!()
+use std::collections::HashSet;
+
+pub fn exchange(n: usize) -> HashSet<Vec<usize>> {
+    let mut acc = HashSet::new();
+    exchange_internal(get_values(n), &mut acc);
+
+    let mut res = HashSet::new();
+    for values in acc {
+        res.insert([vec![1; values[0]], vec![5; values[1]], vec![10; values[2]]].concat());
+    }
+    res
+}
+
+fn get_values(n: usize) -> [usize; 3] {
+    [
+        n % 5,
+        (n % 10) / 5,
+        n / 10,
+    ]
+}
+
+// acc = [count_of_1, count_of_5, count_of_10]
+fn exchange_internal(values: [usize; 3], acc: &mut HashSet<[usize; 3]>) {
+    if values[2] > 0 {
+        let mut values = values.clone();
+        values[2] -= 1;
+        values[1] += 2;
+        acc.insert(values);
+        exchange_internal(values, acc);
+    }
+
+    if values[1] > 0 {
+        let mut values = values.clone();
+        values[1] -= 1;
+        values[0] += 5;
+        acc.insert(values);
+        exchange_internal(values, acc);
+    }
+
+    acc.insert(values);
 }
 
 pub fn exchange_1_5_10_20_50(n: usize) -> Vec<usize> {
@@ -14,26 +52,45 @@ mod tests {
     #[rustfmt::skip]
     fn exchange_test() {
         assert_eq!(
+            exchange(15),
+            {
+                let mut set = HashSet::new();
+                set.insert(vec![5, 10]);
+                set.insert(vec![5, 5, 5]);
+                set.insert(vec![1, 1, 1, 1, 1, 10]);
+                set.insert(vec![1, 1, 1, 1, 1, 5, 5]);
+                set.insert(vec![1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5]);
+                set.insert(vec![1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
+                set
+            }
+        );
+        assert_eq!(
             exchange(10),
-            vec![
-                vec![10],
-                vec![5, 5],
-                vec![1, 1, 1, 1, 1, 5],
-                vec![1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-            ],
+            {
+                let mut set = HashSet::new();
+                set.insert(vec![10]);
+                set.insert(vec![5, 5]);
+                set.insert(vec![1, 1, 1, 1, 1, 5]);
+                set.insert(vec![1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
+                set
+            }
         );
         assert_eq!(
             exchange(1),
-            vec![
-                vec![1],
-            ],
+            {
+                let mut set = HashSet::new();
+                set.insert(vec![1]);
+                set
+            }
         );
         assert_eq!(
             exchange(5),
-            vec![
-                vec![5],
-                vec![1, 1, 1, 1, 1],
-            ],
+            {
+                let mut set = HashSet::new();
+                set.insert(vec![5]);
+                set.insert(vec![1, 1, 1, 1, 1]);
+                set
+            }
         );
     }
 
