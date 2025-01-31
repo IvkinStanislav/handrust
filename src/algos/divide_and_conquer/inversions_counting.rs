60664bd340e70118a4ecc43962c99cadfa981dca
Stas Ivkin <109stas@gmail.com>
2025-01-31 11:08:53 +0300

    WIP inversions_counting

--- src/algos/divide_and_conquer/inversions_counting.rs
+++ src/algos/divide_and_conquer/inversions_counting.rs
@@ -1,39 +1,55 @@
-use std::iter::Peekable;
-
-pub fn inversions_counting(data: Vec<usize>) -> usize {
-    todo!()
-}
-
-struct MergeIterator<I: Iterator> {
-    iter1: Peekable<I>,
-    iter2: Peekable<I>,
-}
-
-impl<I: Iterator> MergeIterator<I> {
-    fn new<II: IntoIterator<IntoIter = I>>(source1: II, source2: II) -> Self {
-        Self {
-            iter1: source1.into_iter().peekable(),
-            iter2: source2.into_iter().peekable(),
-        }
+pub fn inversions_counting(data: &mut Vec<usize>) -> usize {
+    if data.len() <= 1 {
+        return 0;
     }
+
+    let (left, right) = data.split_at(data.len() / 2);
+    let (mut left, mut right) = (left.to_vec(), right.to_vec());
+
+    let left_inversions = inversions_counting(&mut left);
+    let right_inversions = inversions_counting(&mut right);
+    let (new_data, merge_inversions) = merge(&left, &right);
+    *data = new_data;
+
+    left_inversions + right_inversions + merge_inversions
 }
 
-impl<T: Ord, I: Iterator<Item = T>> Iterator for MergeIterator<I> {
-    type Item = I::Item;
+fn merge(left: &[usize], right: &[usize]) -> (Vec<usize>, usize) {
+    use std::cmp::Ordering::*;
 
-    fn next(&mut self) -> Option<Self::Item> {
-        use std::cmp::Ordering::*;
+    let mut result = Vec::with_capacity(left.len() + right.len());
+    let mut left_len = left.len();
+    let mut inversions_count = 0;
+    let (mut left, mut right) = (left.iter(), right.iter());
+    let (mut l_next, mut r_next) = (left.next(), right.next());
 
-        match (self.iter1.peek(), self.iter2.peek()) {
-            (Some(e1), Some(e2)) => match e1.cmp(&e2) {
-                Less | Equal => self.iter1.next(),
-                Greater => self.iter2.next(),
+    loop {
+        match (l_next, r_next) {
+            (Some(&l), Some(&r)) => match l.cmp(&r) {
+                Less | Equal => {
+                    result.push(l);
+                    l_next = left.next();
+                    left_len -= 1;
+                }
+                Greater => {
+                    inversions_count += left_len;
+                    result.push(r);
+                    r_next = right.next();
+                }
             },
-            (Some(_), None) => self.iter1.next(),
-            (None, Some(_)) => self.iter2.next(),
-            (None, None) => None,
+            (Some(&l), None) => {
+                result.push(l);
+                l_next = left.next();
+            }
+            (None, Some(&r)) => {
+                result.push(r);
+                r_next = right.next();
+            }
+            (None, None) => break,
         }
     }
+
+    (result, inversions_count)
 }
 
 pub fn semi_inversion_counting(data: Vec<usize>) -> usize {
@@ -56,9 +72,14 @@ mod tests {
 
     #[test]
     fn inversions_counting_test() {
-        assert_eq!(inversions_counting(vec![2, 3, 9, 2, 9]), 2);
-        assert_eq!(inversions_counting(vec![1, 1, 1]), 0);
-        assert_eq!(inversions_counting(vec![1, 2, 1, 2, 1, 2, 3, 1, 3, 1]), 12);
+        assert_eq!(inversions_counting(&mut vec![2, 3, 9, 2]), 2);
+        assert_eq!(inversions_counting(&mut vec![1, 1, 1]), 0);
+        assert_eq!(inversions_counting(&mut vec![5, 1, 4, 2, 3, 6, 7, 9, 8]), 7);
+        assert_eq!(inversions_counting(&mut vec![5, 1, 4]), 2);
+        assert_eq!(
+            inversions_counting(&mut vec![1, 2, 1, 2, 1, 2, 3, 1, 3, 1]),
+            12
+        );
     }
 
     #[test]
--- src/algos/divide_and_conquer/mod.rs
+++ src/algos/divide_and_conquer/mod.rs
@@ -3,3 +3,35 @@ pub mod dominant_element_search;
 pub mod inversions_counting;
 pub mod modification_of_quicksort;
 pub mod nearest_points_pair;
+
+pub struct MergeIterator<I: Iterator> {
+    iter1: std::iter::Peekable<I>,
+    iter2: std::iter::Peekable<I>,
+}
+
+impl<I: Iterator> MergeIterator<I> {
+    pub fn new<II: IntoIterator<IntoIter = I>>(source1: II, source2: II) -> Self {
+        Self {
+            iter1: source1.into_iter().peekable(),
+            iter2: source2.into_iter().peekable(),
+        }
+    }
+}
+
+impl<T: Ord, I: Iterator<Item = T>> Iterator for MergeIterator<I> {
+    type Item = I::Item;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        use std::cmp::Ordering::*;
+
+        match (self.iter1.peek(), self.iter2.peek()) {
+            (Some(e1), Some(e2)) => match e1.cmp(&e2) {
+                Less | Equal => self.iter1.next(),
+                Greater => self.iter2.next(),
+            },
+            (Some(_), None) => self.iter1.next(),
+            (None, Some(_)) => self.iter2.next(),
+            (None, None) => None,
+        }
+    }
+}
