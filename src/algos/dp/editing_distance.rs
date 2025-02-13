17d07083a05309ecc3731e3d60ae0b17ce225e83
Stas Ivkin <109stas@gmail.com>
2025-02-13 11:19:22 +0300

    WIP editing_distance

--- src/algos/dp/editing_distance.rs
+++ src/algos/dp/editing_distance.rs
@@ -1,10 +1,13 @@
+use super::Matrix;
+
 pub fn editing_distance(s1: &str, s2: &str) -> usize {
     let s1: Vec<_> = s1.chars().collect();
     let s2: Vec<_> = s2.chars().collect();
-    editing_distance_internal(&s1, &s2)
+    let mut dp = Matrix::new_with_init_value(usize::MAX, s1.len() + 1, s2.len() + 1);
+    editing_distance_internal(&s1, &s2, &mut dp)
 }
 
-fn editing_distance_internal(s1: &[char], s2: &[char]) -> usize {
+fn editing_distance_internal(s1: &[char], s2: &[char], dp: &mut Matrix<usize>) -> usize {
     let Some((s1_last, s1_remainder)) = s1.split_last() else {
         return s2.len();
     };
@@ -12,12 +15,48 @@ fn editing_distance_internal(s1: &[char], s2: &[char]) -> usize {
         return s1.len();
     };
 
-    let deletion = editing_distance_internal(s1_remainder, s2) + 1;
-    let insertion = editing_distance_internal(s1, s2_remainder) + 1;
-    let replacement = editing_distance_internal(s1_remainder, s2_remainder)
+    let (i, j) = (s1.len() - 1, s2.len() - 1);
+    if dp[i][j] != usize::MAX {
+        return dp[i][j];
+    }
+
+    let deletion = editing_distance_internal(s1_remainder, s2, dp) + 1;
+    let insertion = editing_distance_internal(s1, s2_remainder, dp) + 1;
+    let replacement = editing_distance_internal(s1_remainder, s2_remainder, dp)
         + (s1_last == s2_last).then_some(0).unwrap_or(1);
 
-    replacement.min(insertion).min(deletion)
+    dp[i][j] = replacement.min(insertion).min(deletion);
+    dp[i][j]
+}
+
+#[allow(dead_code)]
+fn editing_distance_rec(s1: &str, s2: &str) -> usize {
+    let s1: Vec<_> = s1.chars().collect();
+    let s2: Vec<_> = s2.chars().collect();
+    let mut dp = Matrix::new_with_init_value(usize::MAX, s1.len(), s2.len());
+    editing_distance_rec_internal(&s1, &s2, &mut dp)
+}
+
+fn editing_distance_rec_internal(s1: &[char], s2: &[char], dp: &mut Matrix<usize>) -> usize {
+    let Some((s1_last, s1_remainder)) = s1.split_last() else {
+        return s2.len();
+    };
+    let Some((s2_last, s2_remainder)) = s2.split_last() else {
+        return s1.len();
+    };
+
+    let (i, j) = (s1.len() - 1, s2.len() - 1);
+    if dp[i][j] != usize::MAX {
+        return dp[i][j];
+    }
+
+    let deletion = editing_distance_rec_internal(s1_remainder, s2, dp) + 1;
+    let insertion = editing_distance_rec_internal(s1, s2_remainder, dp) + 1;
+    let replacement = editing_distance_rec_internal(s1_remainder, s2_remainder, dp)
+        + (s1_last == s2_last).then_some(0).unwrap_or(1);
+
+    dp[i][j] = replacement.min(insertion).min(deletion);
+    dp[i][j]
 }
 
 #[cfg(test)]
--- src/algos/dp/mod.rs
+++ src/algos/dp/mod.rs
@@ -1,3 +1,5 @@
+use std::ops::{Index, IndexMut, Range};
+
 pub mod backpack;
 pub mod calculator;
 pub mod editing_distance;
@@ -5,3 +7,40 @@ pub mod exchange;
 pub mod lcs;
 pub mod place_brackets;
 pub mod souvenirs;
+
+pub struct Matrix<T> {
+    data: Vec<T>,
+    row_len: usize
+}
+
+impl<T: Clone> Matrix<T> {
+    pub fn new_with_init_value(init_value: T, n: usize, m: usize) -> Self {
+        Self {
+            data: vec![init_value; n * m],
+            row_len: m,
+        }
+    }
+}
+
+impl<T> Matrix<T> {
+    fn get_row_range(&self, row_index: usize) -> Range<usize> {
+        row_index * self.row_len .. (row_index + 1) * self.row_len
+    }
+}
+
+impl<T> Index<usize> for Matrix<T> {
+    type Output = [T];
+
+    fn index(&self, index: usize) -> &Self::Output {
+        let row_range = self.get_row_range(index);
+        &self.data[row_range]
+    }
+}
+
+
+impl<T> IndexMut<usize> for Matrix<T> {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        let row_range = self.get_row_range(index);
+        &mut self.data[row_range]
+    }
+}
